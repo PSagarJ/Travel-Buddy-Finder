eff8a1ed186ea2610e5aa1c130026af9295cb4a3
PSagarJ <sagarjp369@gmail.com>
2026-07-10 11:43:35 +0530

    Corrected the miss-spelled folder name

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import { createPost, getAllPosts, getUserPosts, getTripPosts, deletePost } from '../controllers/postController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
